12fbf1fcd69215f94a3bff9ae1a1649c48de3d96
Alexey Petin <alex_beholder@mail.ru>
2023-03-07 13:08:06 +0300

    Release

--- src/test/java/ru.netology/MessageSenderTests.java
+++ src/test/java/ru.netology/MessageSenderTests.java
@@ -1,15 +1,36 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.sender.MessageSender;
 import ru.netology.sender.MessageSenderImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MessageSenderTests {
     @Test
     void MessageSenderImplWithMockito(){
-        GeoServiceMock geoServiceMock = new GeoServiceMock();
-        GeoService geoService = Mockito.mock(GeoService.class);
-//        Mockito.when(geoService.byIp())
+        GeoService geoServiceMock = Mockito.mock(GeoService.class);
+        Mockito.when(geoServiceMock.byIp("96.44.183.149"))
+                .thenReturn(new Location("New York", Country.USA, null, 0));
+
+        LocalizationService preferenceLocalization = Mockito.mock(LocalizationService.class);
+        Mockito.when(preferenceLocalization.locale(Country.USA))
+                .thenReturn("Welcome");
+
+        MessageSender messageSender = new MessageSenderImpl(geoServiceMock, preferenceLocalization);
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("x-real-ip", "96.44.183.149");
+        String returned = messageSender.send(headers);
+        String expected = "Welcome";
+
+        Assertions.assertEquals(expected, returned);
     }
 }
